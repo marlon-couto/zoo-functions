a87c8874a76b28602fa1be37896caa23615d4ca8
Marlon Couto <marloncouto199@gmail.com>
2022-10-31 22:37:50 -0300

    Finaliza requisito 4
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,8 +3,9 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 function getAnimalsOlderThan(animal, age) {
-  const residents = species.find(({ name }) => name === animal);
-  return residents.residents.every((resident) => resident.age >= age);
+  return species
+    .find(({ name }) => name === animal)
+    .residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
