9a6254c88c890932310beef681f71da91f5454c3
Marlon Couto <marloncouto199@gmail.com>
2022-11-01 15:55:52 -0300

    Finaliza o requisito 8
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter(({ age }) => age < 18).length;
+  const adult = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const senior = entrants.filter(({ age }) => age >= 50).length;
+
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.entries(entrants).length === 0) return 0;
+  const count = Object.values(countEntrants(entrants));
+  return prices.child * count[0] + prices.adult * count[1] + prices.senior * count[2];
 }
 
 module.exports = { calculateEntry, countEntrants };
