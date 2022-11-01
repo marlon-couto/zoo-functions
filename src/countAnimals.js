03bfbd098954d4dabf0901c5df1ea6716fcf29c2
Marlon Couto <marloncouto199@gmail.com>
2022-11-01 01:16:48 -0300

    Finaliza requisito 5
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const allSpecies = species.map(({ name, residents }) => ({
+    [name]: residents.length,
+  }));
+
+  if (!animal) {
+    return Object.fromEntries(
+      Object.entries(Object.assign({}, ...allSpecies)).sort(),
+    );
+  }
+  if (!animal.sex) {
+    return Object.values(
+      allSpecies.find((specie) => Object.keys(specie).includes(animal.specie)),
+    )[0];
+  }
+  return species
+    .find(({ name }) => name === animal.specie)
+    .residents.filter(({ sex }) => sex === 'female').length;
 }
 
 module.exports = countAnimals;
