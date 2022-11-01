9fae55a4ce5a8d8cd534bf5d35626f43ce4e7622
Marlon Couto <marloncouto199@gmail.com>
2022-11-01 18:46:01 -0300

    Finaliza requisito 9
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,47 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+
+const exhibitionList = (day) =>
+  species
+    .filter((specie) => specie.availability.includes(day))
+    .map((animal) => animal.name);
+
+const hourGrade = () =>
+  Object.assign(
+    {},
+    ...Object.keys(hours).map((day) => {
+      const { open, close } = hours[day];
+      return {
+        [day]: {
+          officeHour:
+            day !== 'Monday'
+              ? `Open from ${open}am until ${close}pm`
+              : 'CLOSED',
+          exhibition:
+            day !== 'Monday' ? exhibitionList(day) : 'The zoo will be closed!',
+        },
+      };
+    }),
+  );
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const grade = hourGrade();
+  const validSpecie = species.some((specie) => Object.values(specie).includes(scheduleTarget));
+  const validDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  if (!scheduleTarget) return grade;
+
+  if (validSpecie) {
+    return species.find((specie) => specie.name === scheduleTarget)
+      .availability;
+  }
+  if (validDays.includes(scheduleTarget)) {
+    return {
+      scheduleTarget: grade[scheduleTarget],
+    };
+  }
+  return grade;
 }
 
 module.exports = getSchedule;
