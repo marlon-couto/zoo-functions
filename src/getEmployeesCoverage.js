3b6d2d5747aa0106a769cdc2a4aba492e3e0cf3a
Marlon Couto <marloncouto199@gmail.com>
2022-11-02 02:18:40 -0300

    Atualiza requisito 11
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,55 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+const verifyEmployee = (searchParam) => {
+  const { name } = searchParam;
+
+  if (
+    !employees.some(
+      ({ firstName, lastName, id }) =>
+        firstName === name || lastName === name || id === searchParam.id,
+    )
+  ) {
+    throw new Error('Informações inválidas');
+  }
+};
+
+const findEmployee = (searchParam) => {
+  if (!searchParam) return employees;
+
+  const { name, id } = searchParam;
+
+  if (name) {
+    return employees.filter(
+      ({ firstName, lastName }) => firstName === name || lastName === name,
+    );
+  }
+  if (id) return employees.filter((employee) => employee.id === id);
+};
+
+const findAnimals = ({ responsibleFor }) =>
+  species.filter(({ id }) => responsibleFor.some((index) => index === id));
+
+function getEmployeesCoverage(searchParam) {
+  try {
+    if (searchParam) {
+      verifyEmployee(searchParam);
+    }
+
+    const coverage = findEmployee(searchParam).map((employee) => ({
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: findAnimals(employee).map((animal) => `${animal.name}`),
+      locations: findAnimals(employee).map((animal) => `${animal.location}`),
+    }));
+
+    return coverage.length > 1 ? coverage : coverage.find((only) => only);
+  } catch (error) {
+    return error.message;
+  }
 }
 
 module.exports = getEmployeesCoverage;
+
+console.log(getEmployeesCoverage({ id: 'Id inválido' }));
